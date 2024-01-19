1de29fb75f02a64cd7655df7f083d8d860198d49
shantanu-bandarkar <shantanubandarkar@gmail.com>
2024-01-19 21:34:55 +0530

    part1-b JS
    Exercise 1.3 - 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,22 +1,59 @@
-import Header from "./Header"
-import Content from "./Content"
-import Total from "./Total"
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+const Header = (props) => {
+    return (
+      <h1>{props.course}</h1>
+    )
+  }
+  
+  const Part = (props) => {
+    return (
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    )
+  }
+  
+  const Content = (props) => {
+    return (
+      <div>
+        <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
+        <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
+        <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
+      </div>
+    )
+  }
+  
+  const Total = (props) => {
+    return (
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    )
+  }
+  
+  const App = () => {
+    const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+    }
+  
+    return (
+      <div>
+        <Header course={course.name} />
+        <Content parts={course.parts}/>
+        <Total parts={course.parts} />
+      </div>
+    )
+  }
 
-  return (
-    <>
-      <Header course={course}/>
-      <Content pt1={part1} ex1={exercises1} pt2={part2} ex2={exercises2} pt3={part3} ex3={exercises3} />
-      <Total ex1={exercises1} pt2={part2} ex2={exercises2} pt3={part3} ex3={exercises3}/>
-    </>
-  )
-}
-
-export default App;
+  export default App;
--- part1/courseinfo/src/Part.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Part = (props) => {
-  return (
-    <p>{props.pt} {props.ex}</p>
-  )
-}
-
-export default Part
