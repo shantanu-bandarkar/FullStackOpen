4ec3e0335b925e4fae941d2542591ea8f4370f0a
shantanu-bandarkar <shantanubandarkar@gmail.com>
2024-02-03 19:31:26 +0530

    exercises 3.15 - 3.18

--- part3/phonebook backend/index.js
+++ part3/phonebook backend/index.js
@@ -7,7 +7,6 @@ app.use(express.json());
 app.use(express.static("dist"));
 const morgan = require("morgan");
 const Person = require("./model/person");
-const { log } = require("console");
 
 // Define a custom token to display request body
 morgan.token("tiny", function (req, res) {
@@ -18,6 +17,18 @@ morgan.token("tiny", function (req, res) {
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :tiny")
 );
+
+morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, "content-length"),
+    "-",
+    tokens["response-time"](req, res),
+    "ms",
+  ].join(" ");
+});
 
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((result) => {
@@ -34,36 +45,37 @@ app.get("/api/persons/:id", (request, response) => {
     .catch((e) => response.status(404).json({ error: "not found" }));
 });
 
-app.get("/info", (request, response) => {
-  const length = notes.length;
-  response.setHeader("Current-Time", new Date());
-  const dateHeader = response.getHeader("Current-Time");
-  response.send(
-    `<p>Phonebook has info for ${length} people</p><p>${dateHeader}</p>`
-  );
-});
+app.get("/info", async (request, response) => {
+  try {
+    const persons = await Person.find({});
+    const length = persons.length;
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  if (notes.find((n) => n.id === id)) {
-    notes = notes.filter((n) => n.id !== id);
-    return response.status(204).end();
-  } else {
-    response
-      .status(404)
-      .end("<p>Delete Operation failed: Person doesn't exists.");
+    response.setHeader("Current-Time", new Date());
+    const dateHeader = response.getHeader("Current-Time");
+
+    response.send(
+      `<p>Phonebook has info for ${length} people</p><p>${dateHeader}</p>`
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    response.status(500).send("Internal Server Error");
   }
 });
-morgan(function (tokens, req, res) {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, "content-length"),
-    "-",
-    tokens["response-time"](req, res),
-    "ms",
-  ].join(" ");
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const person = {
+    name: request.body.name,
+    number: request.body.number,
+  };
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => response.json(updatedPerson))
+    .catch((error) => next(error));
+});
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => response.status(204).json(result))
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -84,3 +96,23 @@ app.post("/api/persons", (request, response) => {
 const PORT = process.env.PORT;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+// this has to be the last loaded middleware.
+app.use(errorHandler);
